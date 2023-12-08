827cbf9a6fe29da6caeb67c2617857c52db96b67
Alexey Yakimchuk <yakimchuk@gmail.com>
2023-12-08 21:01:49 +0300

    Fixed initialization of memStorage fields

--- repositories/store/memstore.go
+++ repositories/store/memstore.go
@@ -20,21 +20,18 @@ type MemStorage struct {
 
 // NewMemStorage конструктур типа MemStorage
 func NewMemStorage() *MemStorage {
-	return &MemStorage{}
+	return &MemStorage{
+		Counter: map[string]Counter{},
+		Gauge:   map[string]Gauge{},
+	}
 }
 
 // Update описываем метод в соответствии с контактном интерфейсного типа StoreRepository
 func (ms *MemStorage) Update(metric map[string]string) error {
 	switch value := utils.CheckTypeOfMetricValue(metric["value"]).(type) {
 	case int64:
-		if _, ok := ms.Counter[metric["name"]]; !ok {
-			ms.Counter = make(map[string]Counter)
-		}
 		ms.Counter[metric["name"]] += Counter(value)
 	case float64:
-		if _, ok := ms.Counter[metric["name"]]; !ok {
-			ms.Gauge = make(map[string]Gauge)
-		}
 		ms.Gauge[metric["name"]] = Gauge(value)
 	}
 	// log.Printf("%#v", ms)
