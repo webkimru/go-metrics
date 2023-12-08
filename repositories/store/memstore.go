63fb67e2e7040b30c1b481e6468228c849c1a48b
Alexey Yakimchuk <yakimchuk@gmail.com>
2023-12-08 23:02:03 +0300

    Fixed concurrent map iteration and map write

--- repositories/store/memstore.go
+++ repositories/store/memstore.go
@@ -3,7 +3,9 @@ package store
 import (
 	"errors"
 	"github.com/webkimru/go-yandex-metrics/internal/utils"
+	"log"
 	"strconv"
+	"sync"
 )
 
 var (
@@ -17,6 +19,7 @@ type Gauge float64
 type MemStorage struct {
 	Counter map[string]Counter
 	Gauge   map[string]Gauge
+	mu      sync.Mutex
 }
 
 // NewMemStorage конструктур типа MemStorage
@@ -30,14 +33,20 @@ func NewMemStorage() *MemStorage {
 // UpdateCounter обновляем поле Counter
 // описываем метод в соответствии с контактном интерфейсного типа StoreRepository
 func (ms *MemStorage) UpdateCounter(metric map[string]string) error {
+	ms.mu.Lock()
+	defer ms.mu.Unlock()
+
 	value, _ := strconv.ParseInt(metric["value"], 10, 64)
 	ms.Counter[metric["name"]] += Counter(value)
-	// log.Printf("%#v", ms)
+	log.Printf("%#v", ms)
 	return nil
 }
 
 // UpdateGauge обновляем поле UpdateGauge
 func (ms *MemStorage) UpdateGauge(metric map[string]string) error {
+	ms.mu.Lock()
+	defer ms.mu.Unlock()
+
 	switch value := utils.CheckTypeOfMetricValue(metric["value"]).(type) {
 	case int64:
 	case float64:
@@ -45,6 +54,6 @@ func (ms *MemStorage) UpdateGauge(metric map[string]string) error {
 	default:
 		return ErrUpdateFailed
 	}
-	// log.Printf("%#v", ms)
+	log.Printf("%#v", ms)
 	return nil
 }
