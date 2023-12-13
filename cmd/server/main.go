1854040e08f699ea90ff1c71248482f94dffa7c9
Alexey Yakimchuk <yakimchuk@gmail.com>
2023-12-13 10:48:35 +0300

    Added a server flag

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,14 +1,22 @@
 package main
 
-import "net/http"
+import (
+	"flag"
+	"net/http"
+)
 
 // main исходный код программы
 func main() {
+	// указываем имя флага, значение по умолчанию и описание
+	serverAddress := flag.String("a", "localhost:8080", "server address")
+	// разбор командной строки
+	flag.Parse()
+
 	if err := run(); err != nil {
 		panic(err)
 	}
 
 	// стартуем сервер
-	err := http.ListenAndServe(`:8080`, middleware(routes()))
+	err := http.ListenAndServe(*serverAddress, middleware(routes()))
 	panic(err)
 }
