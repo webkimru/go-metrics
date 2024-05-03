bf182fb2240d8eb7e2fe20d61a498bc9e1a20d6b
Alexey Yakimchuk <yakimchuk@gmail.com>
2024-05-03 16:18:43 +0300

    Redefined agent configuration variables by priority env/flag/file

--- internal/app/agent/config/config.go
+++ internal/app/agent/config/config.go
@@ -5,11 +5,11 @@ import (
 )
 
 type AppConfig struct {
-	SecretKey      string
-	ServerAddress  string
-	CryptoKey      string
-	PublicKeyPEM   *rsa.PublicKey
-	RateLimit      int
-	PollInterval   int
-	ReportInterval int
+	SecretKey      string         `json:"key,omitempty"`
+	ServerAddress  string         `json:"address,omitempty"`
+	CryptoKey      string         `json:"crypto_key,omitempty"`
+	PublicKeyPEM   *rsa.PublicKey `json:"-"`
+	RateLimit      int            `json:"rate_limit,omitempty"`
+	PollInterval   int            `json:"poll_interval,omitempty"`
+	ReportInterval int            `json:"report_interval,omitempty"`
 }
--- internal/app/agent/init.go
+++ internal/app/agent/init.go
@@ -1,7 +1,9 @@
 package agent
 
 import (
+	"encoding/json"
 	"flag"
+	"fmt"
 	"github.com/webkimru/go-yandex-metrics/internal/app/agent/config"
 	"github.com/webkimru/go-yandex-metrics/internal/app/agent/logger"
 	"github.com/webkimru/go-yandex-metrics/internal/security"
@@ -12,12 +14,13 @@ import (
 
 func Setup() (int, error) {
 	// задаем флаги для агента
-	serverAddress := flag.String("a", "localhost:8080", "server address")
-	reportInterval := flag.Int("r", 10, "report interval (in seconds)")
-	pollInterval := flag.Int("p", 2, "poll interval (in seconds)")
+	serverAddress := flag.String("a", "", "server address")
+	reportInterval := flag.Int("r", 0, "report interval (in seconds)")
+	pollInterval := flag.Int("p", 0, "poll interval (in seconds)")
 	secretKey := flag.String("k", "", "secret key")
-	rateLimit := flag.Int("l", 1, "rate limit (a number of workers)")
-	cryptoKey := flag.String("crypto-key", "", "path to pem public key")
+	rateLimit := flag.Int("l", 0, "rate limit (a number of workers)")
+	cryptoKey := flag.String("crypto-key", "", "path to pem public key file")
+	configuration := flag.String("c", "", "path to json configuration file")
 
 	// разбор командой строки
 	flag.Parse()
@@ -53,21 +56,67 @@ func Setup() (int, error) {
 	if envCryptoKey := os.Getenv("CRYPTO_KEY"); envCryptoKey != "" {
 		cryptoKey = &envCryptoKey
 	}
-
-	// конфигурация приложения
-	a := config.AppConfig{
-		ServerAddress:  *serverAddress,
-		ReportInterval: *reportInterval,
-		PollInterval:   *pollInterval,
-		SecretKey:      *secretKey,
-		RateLimit:      *rateLimit,
-		CryptoKey:      *cryptoKey,
+	if envConfig := os.Getenv("CONFIG"); envConfig != "" {
+		configuration = &envConfig
 	}
-	app = a
 
 	// инициализируем логер
 	if err := logger.Initialize("info"); err != nil {
 		return 0, err
+	}
+
+	// конфигурация приложения
+	a := config.AppConfig{}
+	app = a
+
+	// читаем конфиг из файла
+	if *configuration != "" {
+		configFile, err := os.ReadFile(*configuration)
+		if err != nil {
+			return 0, fmt.Errorf("failed loading config from file=%s: %w", *configuration, err)
+		}
+		// определяем для всего сервиса конфигурацию из файла
+		if err = json.Unmarshal(configFile, &app); err != nil {
+			return 0, fmt.Errorf("failed unmarshaling config from file=%s: %w", *configuration, err)
+		}
+		if err == nil {
+			logger.Log.Infof("configuration loaded successfully from file=%s", *configuration)
+		}
+	}
+	// переопределяем значения конфига значениями из envs / flags:
+	if *serverAddress != "" {
+		app.ServerAddress = *serverAddress
+	}
+	if *reportInterval != 0 {
+		app.ReportInterval = *reportInterval
+	}
+	if *pollInterval != 0 {
+		app.PollInterval = *pollInterval
+	}
+	if *secretKey != "" {
+		app.SecretKey = *secretKey
+	}
+	if *rateLimit != 0 {
+		app.RateLimit = *rateLimit
+	}
+	if *cryptoKey != "" {
+		app.CryptoKey = *cryptoKey
+	}
+	// обязательные настройки
+	if app.ServerAddress == "" {
+		return 0, fmt.Errorf("destionation server address is not defined, it must be specified, for example, localhost:8080")
+	}
+	if app.RateLimit == 0 {
+		app.RateLimit = 1 // silent default
+		logger.Log.Infof("default rate limit is automatically set = %d", app.RateLimit)
+	}
+	if app.PollInterval == 0 {
+		app.PollInterval = 2 // silent default
+		logger.Log.Infof("default poll interval is automatically set = %d", app.PollInterval)
+	}
+	if app.ReportInterval == 0 {
+		app.ReportInterval = 10 // silent default
+		logger.Log.Infof("default report interval is automatically set = %d", app.ReportInterval)
 	}
 
 	logger.Log.Infoln(
@@ -77,6 +126,7 @@ func Setup() (int, error) {
 		"POLL_INTERVAL", app.PollInterval,
 		"KEY", app.SecretKey,
 		"CRYPTO_KEY", app.CryptoKey,
+		"RATE_LIMIT", app.RateLimit,
 	)
 
 	// инициализация ключей ассиметричного шифрования
